fb650cb302b94c180b7669f22bed13994fd26acd
WayKawaii <duyduyyud@yahoo.com>
2016-02-01 19:53:31 -0600

    Respawn script and lvl1

--- Assets/_Scripts/DoorScript.cs
+++ Assets/_Scripts/DoorScript.cs
@@ -3,9 +3,25 @@ using System.Collections;
 
 public class DoorScript : MonoBehaviour
 {
-    public Sprite doorOpen;
-	void Activate()
+    public Sprite door, doorOpen;
+    public bool enter = false;
+    public string sceneCounter;
+    void Activate()
     {
         GetComponent<SpriteRenderer>().sprite = doorOpen;
+        enter = true;
+    }
+    void Deactivate()
+    {
+        GetComponent<SpriteRenderer>().sprite = door;
+        enter = false;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player") && enter == true)
+        {
+            Application.LoadLevel(sceneCounter);
+        }
     }
 }
---
+++ Assets/_Scripts/Timed_Lever.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timed_Lever : MonoBehaviour
+{
+    public GameObject activator;
+    public Sprite pulled;
+    public Sprite Lever;
+    private bool pulledLever;
+    public float  eventTime;
+    private float closeDoorTimer;
+    // Use this for initialization
+    void Start()
+    {
+        pulledLever = false;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player") && !pulledLever)
+        {
+            activator.SendMessage("Activate");
+            pulledLever = true;
+            closeDoorTimer = Time.time + eventTime;
+            GetComponent<SpriteRenderer>().sprite = pulled;
+        }
+
+    }
+    void Update()
+    {
+        if (Time.time > closeDoorTimer)
+        {
+            activator.SendMessage("Deactivate");
+            pulledLever = false;
+            GetComponent<SpriteRenderer>().sprite = Lever;
+        }
+    }
+}
